94b5c41db48131bd5ba04ebfadc3c9be84e612b5
azahar24 <azaharuddin454@gmail.com>
2022-10-14 19:04:21 +0600

    Initial commit

--- lib/ui/route/views/nav_page/all_pakage.dart
+++ lib/ui/route/views/nav_page/all_pakage.dart
@@ -1,22 +1,30 @@
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get.dart';
 
-import '../../../../const/app_colors.dart';
-import '../../styles/styles.dart';
 
-class AllPakage extends StatelessWidget {
+
+class AllPakage extends StatefulWidget {
+
+  @override
+  State<AllPakage> createState() => _AllPakageState();
+}
+
+class _AllPakageState extends State<AllPakage> {
+  String name = "";
 
   final Stream<QuerySnapshot<Map<String, dynamic>>> _usersStream =
-      FirebaseFirestore.instance
-          .collection('pakage')
-          .doc(FirebaseAuth.instance.currentUser!.email)
-          .collection('product')
-          .snapshots();
+  FirebaseFirestore.instance
+      .collection('pakage')
+      .doc(FirebaseAuth.instance.currentUser!.email)
+      .collection('product')
+      .snapshots();
 
-          Future<void> deleateItem(selectDocument) {
+  Future<void> deleateItem(selectDocument) {
     return FirebaseFirestore.instance
         .collection('pakage')
         .doc(FirebaseAuth.instance.currentUser!.email)
@@ -27,59 +35,206 @@ class AllPakage extends StatelessWidget {
         .catchError((error) => print(error));
   }
 
+  Future _delateDialog(context, selectDocument) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Are u sure to delate this pakage?"),
+            content: Row(
+              children: [
+                ElevatedButton(
+                  onPressed: ()=>Get.back(),
+                  child: Text("No"),
+                ),
+                SizedBox(
+                  width: 20.w,
+                ),
+                ElevatedButton(
+                  onPressed: (){
+                    deleateItem(selectDocument);
+                    Get.back();
+                  },
+                  child: Text("Yes"),
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: AppColors.scaffoldColor,
-        body: StreamBuilder<QuerySnapshot>(
+          appBar: AppBar(
+              title: Card(
+                child: TextField(
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search), hintText: 'Search...'),
+                  onChanged: (val) {
+
+                    setState((){
+                      name = val;
+                    });
+                  },
+                ),
+              )),
+          body: StreamBuilder<QuerySnapshot>(
             stream: _usersStream,
-            builder:
-                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (snapshot.hasError) {
-                return Center(child: Text('Something went wrong'));
-              }
+            builder: (context, snapshots) {
+              return (snapshots.connectionState == ConnectionState.waiting)
+                  ? Center(
+                child: CircularProgressIndicator(),
+              )
+                  : ListView.builder(
+                  itemCount: snapshots.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    var data = snapshots.data!.docs[index].data()
+                    as Map<String, dynamic>;
 
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(child: CircularProgressIndicator());
-              }
-
-              return ListView(
-                children: snapshot.data!.docs.map((DocumentSnapshot document) {
-                  Map<String, dynamic> data =
-                      document.data()! as Map<String, dynamic>;
-                  return Container(
-                    height: 130.h,
-                    child: Card(
-                      child: Column(
-                        children: [
-                          Text(
-                            data['title'],
-                            style: AppStyle().myTiteStyle,
-                          ),
-                          Row(
+                    if (name.isEmpty) {
+                      return Card(
+                        child: Container(
+                          padding: EdgeInsets.only(top: 5.h, left: 10.w, right: 10.w),
+                          height: 120.h,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Text(
-                                'Totall pis : ${data['item2']}',
-                                style: AppStyle().mySmallStyle,
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    data['title'],
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                        color: Colors.black54,
+                                        fontSize: 22.sp,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(height: 10.h,),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Buy',
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                            color: Colors.black54,
+                                            fontSize: 20.sp,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      SizedBox(width: 5.w,),
+                                      Text(
+                                        '=',
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                            color: Colors.black54,
+                                            fontSize: 20.sp,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      SizedBox(width: 5.w,),
+                                      Text(
+                                        data['buyrate'].toString(),
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                            color: Colors.black54,
+                                            fontSize: 20.sp,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(height: 10.h,),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Item',
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                            color: Colors.black54,
+                                            fontSize: 20.sp,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      SizedBox(width: 5.w,),
+                                      Text(
+                                        '=',
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                            color: Colors.black54,
+                                            fontSize: 20.sp,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      SizedBox(width: 5.w,),
+                                      Text(
+                                        data['item2'].toString(),
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                            color: Colors.black54,
+                                            fontSize: 20.sp,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                ],
                               ),
-                              SizedBox(width: 20.w,),
-                              Text(
-                            'Buy : ${data['buyrate']}',
-                            style: AppStyle().mySmallStyle,
-                          ),
+                              Column(
+                                children: [
+                                  IconButton(onPressed: (){}, icon: Icon(Icons.edit,size: 25.h,),),
+                                  IconButton(onPressed: ()=>_delateDialog(context, snapshots.data!.docs[index].id), icon: Icon(Icons.delete,size: 25.h,),),
+                                ],
+                              )
                             ],
                           ),
-                          IconButton(onPressed: () => deleateItem(document.id), icon: Icon(Icons.delete))
-                        ],
-                      ),
-                    ),
-                  );
-                }).toList(),
-              );
-            }),
-      ),
+                        ),
+                      );
+                    }
+                    if (data['title']
+                        .toString()
+                        .toLowerCase()
+                        .startsWith(name.toLowerCase())) {
+                      return Card(
+                        child: Container(
+                          height: 100.h,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                data['title'],
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    color: Colors.black54,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                data['buyrate'].toString(),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    color: Colors.black54,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    }
+                    return Container(
+                    );
+                  });
+            },
+          )),
     );
-  
   }
 }
+
